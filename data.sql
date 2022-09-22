5393c2bbdb40da5e05153d2fb2248a79dbc188b1
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-22 17:02:28 +0430

    Inserted data to animals table

---
+++ data.sql
@@ -0,0 +1,7 @@
+INSERT INTO animals ( name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ( 'Agumon', '2-3-2020', 0, true, 10.23);
+
+INSERT INTO animals ( name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ( 'Gabumon', '9-15-2018', 5, true, 11);
+
+INSERT INTO animals ( name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ( 'Pikachu', '1-7-2021', 1, true, 15.04);
+
+INSERT INTO animals ( name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ( 'Devimon', '5-12-2017', 5, true, 11);
