0899d7501fd63105569c6ad39f7a7bdf1a306862
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-09-22 18:49:22 +0430

    the table displayed in different lines

--- schema.sql
+++ schema.sql
@@ -5,8 +5,8 @@ Username [postgres]:
 Password for user postgres:
 psql (14.5)
 WARNING: Console code page (437) differs from Windows code page (1252)
-        8-bit characters might not work correctly. See psql reference
-        page "Notes for Windows users" for details.
+  8-bit characters might not work correctly. See psql reference
+  page "Notes for Windows users" for details.
 Type "help" for help.
 
 postgres=# create database vet_clinic;
@@ -14,6 +14,13 @@ CREATE DATABASE
 postgres=# \c vet_clinic
 You are now connected to database "vet_clinic" as user "postgres".
 
-vet_clinic=# CREATE TABLE animals (id INT GENERATED ALWAYS AS IDENTITY, name varchar, date_of_birth date, escape_attempts integer, neutered boolean, weight_kg decimal);
+vet_clinic=# CREATE TABLE animals (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name varchar(255),
+  date_of_birth date,
+  escape_attempts integer,
+  neutered boolean,
+  weight_kg decimal
+);
 CREATE TABLE
 vet_clinic=#
